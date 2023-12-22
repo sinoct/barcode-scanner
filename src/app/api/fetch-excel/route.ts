e8d7402def1e7af714e9924d7fdd8bfeaa763748
Gergő Erős <eros.gergo@codeyard.eu>
2023-12-22 15:34:22 +0100

    using env for url

--- src/app/api/fetch-excel/route.ts
+++ src/app/api/fetch-excel/route.ts
@@ -2,14 +2,11 @@ import { NextRequest, NextResponse } from "next/server";
 import * as xlsx from "xlsx";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const file = await fetch(
-    "https://app.clouderp.hu/api/1/automatism/file-share/?s=Z0FBQUFBQmxnWjE4SjhrWS1TbmVGYnk2UGtiTU5mQ1JXd0V0clUxR1I2a2pWQjJuYUFRaGZBX1VmT0RfelljUkZmY1g1R3JiMWZuYnE2TUt2d3hfam9TZldMVGpHSzFZdFJFUmdNV3RxV2dzaUpxNTlGYmhWbVU9&f=xlsx",
-    {
-      headers: {
-        "Content-Type": "application/ms-excel;",
-      },
-    }
-  );
+  const file = await fetch(process.env.NEXT_PUBLIC_EXCEL_SOURCE_URL, {
+    headers: {
+      "Content-Type": "application/ms-excel;",
+    },
+  });
   const arrayBuffer = await file.arrayBuffer();
   const buffer = new Uint8Array(arrayBuffer);
 
