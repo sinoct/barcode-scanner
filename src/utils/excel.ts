7ffbd50430104527e8b30bec7a2c3d559dcdea4c
Gergo Eros <gergo.eros@theitsolutions.io>
2024-02-03 14:47:38 +0100

    set caching headers

--- src/utils/excel.ts
+++ src/utils/excel.ts
@@ -1,8 +1,12 @@
 export const fetchPriceList = async () => {
   const res = await fetch("/api/fetch-excel", {
+    headers: {
+      "Cache-Control": "no-cache, no-store, max-age=0, must-revalidate",
+    },
     method: "GET",
     cache: "no-cache",
   });
   const content = await res.text();
+
   return JSON.parse(content);
 };
