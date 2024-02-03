cabf11bb4c1af8f45ffe6e9bdaf4d1ed66ecd533
Gergo Eros <gergo.eros@theitsolutions.io>
2024-02-03 14:36:58 +0100

    disabled cacheing

--- src/utils/excel.ts
+++ src/utils/excel.ts
@@ -1,6 +1,7 @@
 export const fetchPriceList = async () => {
   const res = await fetch("/api/fetch-excel", {
     method: "GET",
+    cache: "no-cache",
   });
   const content = await res.text();
   return JSON.parse(content);
