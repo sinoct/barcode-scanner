d9c69200553217a984276fba8072ad3b78b004c9
Gergo Eros <gergo.eros@theitsolutions.io>
2024-02-07 17:09:27 +0100

    using env variable for source and updated price rounding

--- src/app/api/fetch-excel/route.ts
+++ src/app/api/fetch-excel/route.ts
@@ -2,20 +2,23 @@ import { NextRequest, NextResponse } from "next/server";
 import * as xlsx from "xlsx";
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const file = await fetch(
-    "https://app.clouderp.hu/api/1/automatism/file-share/?s=Z0FBQUFBQmxnWjE4SjhrWS1TbmVGYnk2UGtiTU5mQ1JXd0V0clUxR1I2a2pWQjJuYUFRaGZBX1VmT0RfelljUkZmY1g1R3JiMWZuYnE2TUt2d3hfam9TZldMVGpHSzFZdFJFUmdNV3RxV2dzaUpxNTlGYmhWbVU9&f=xlsx",
-    {
+  const url = process.env.NEXT_PUBLIC_EXCEL_SOURCE_URL;
+  if (url) {
+    const file = await fetch(url, {
       headers: {
         "Content-Type": "application/ms-excel;",
       },
-    }
-  );
-  const arrayBuffer = await file.arrayBuffer();
-  const buffer = new Uint8Array(arrayBuffer);
+    });
+    const arrayBuffer = await file.arrayBuffer();
+    const buffer = new Uint8Array(arrayBuffer);
 
-  const workbook = xlsx.read(buffer, { type: "array" });
-  const worksheet = workbook.Sheets[workbook.SheetNames[0]];
-  const json = xlsx.utils.sheet_to_json(worksheet);
-
-  return new Response(JSON.stringify(json));
+    const workbook = xlsx.read(buffer, { type: "array" });
+    const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+    const json = xlsx.utils.sheet_to_json(worksheet);
+    return new Response(JSON.stringify(json));
+  } else {
+    return new Response(
+      JSON.stringify({ message: "File could not be fetched" })
+    );
+  }
 }
--- src/utils/printHelper.ts
+++ src/utils/printHelper.ts
@@ -1,7 +1,7 @@
 import jsPDF, { jsPDFOptions } from "jspdf";
 
 export const printPrice = (price: string) => {
-  const formattedPrice = Math.round(parseInt(price));
+  const formattedPrice = Math.round(parseInt(price) / 10) * 10;
   const width = formattedPrice.toString().length * 7 + 28;
   const formatArray = [width, 20];
   const doc = new jsPDF({
